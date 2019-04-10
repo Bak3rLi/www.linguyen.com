bc298056ddc121db78e3ff376358c4cfc6f588da
Bak3rLi <49377017+Bak3rLi@users.noreply.github.com>
2019-04-10 21:38:22 +0100

    Update extras.js

--- assets/js/extras.js
+++ assets/js/extras.js
@@ -1,19 +1,13 @@
-function showMenu(menuOption) {
-  $('.sub-menu').hide();
-  menuOption.nextUntil("main-menu").slideDown( "fast", function() {
-    // Animation complete.
-  });
-}
-
 function get_filename(url) {
   return url.split('#').shift().split('?').shift().split('/').pop();
 }
 
 function initMenu() {
   current_page = get_filename(document.location.pathname);
-  alert(current_page);
   $('.main-menu').each(function(i) {
-    alert($(this).children("a"))
-    alert("HREF:" + get_filename($(this).children("a")[0].href));
+    href = get_filename($(this).children("a")[0].href)
+    if (href == current_page) {
+      $(this).nextUntil("main-menu").slideDown("fast");
+    }
   });
 }
