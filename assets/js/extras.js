07f48a551e82164250b9766242ac6903aad93fc4
Bak3rLi <49377017+Bak3rLi@users.noreply.github.com>
2019-04-10 21:51:51 +0100

    Update extras.js

--- assets/js/extras.js
+++ assets/js/extras.js
@@ -2,12 +2,12 @@ function get_filename(url) {
   return url.split('#').shift().split('?').shift().split('/').pop();
 }
 
-function initMenu() {
+function initMenu(menu) {
   current_page = get_filename(document.location.pathname);
   $('.main-menu').each(function(i) {
     href = get_filename($(this).children("a")[0].href)
     if (href == current_page) {
-      $(this).nextUntil("main-menu").slideDown("fast");
+      $(this).nextUntil("main-menu").show();
     }
   });
 }
